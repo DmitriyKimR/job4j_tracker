ac7b266256345cf22f4912c954ac4ff634f6ae47
Funny-Buny <incoming@mail.ru>
2021-06-29 11:59:49 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+        public void printInfo() {
+            System.out.println("The presence of error: " + active);
+            System.out.println("Error status: " + status);
+            System.out.println("Error: " + message);
+        }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 2, "Find the error");
+        Error start = new Error();
+        error.printInfo();
+        start.printInfo();
+    }
+
+    public Error() {
+    }
+}
