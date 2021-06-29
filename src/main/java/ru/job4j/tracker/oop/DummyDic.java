3455384014dfd1c04daebae8bd54ddc034482f3b
Funny-Buny <incoming@mail.ru>
2021-06-29 08:47:30 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387 #172316]

--- src/main/java/ru/job4j/tracker/oop/DummyDic.java
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -2,12 +2,12 @@ package ru.job4j.tracker.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        return eng;
+        return "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic variant = new DummyDic();
-        String word = variant.engToRus("wallet");
-        System.out.println("Неизвестное слово. " + word);
+        String result = variant.engToRus("wallet");
+        System.out.println(result);
     }
 }
