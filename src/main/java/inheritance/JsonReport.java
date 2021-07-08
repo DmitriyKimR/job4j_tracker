2fcb60b44a5164feb4678fbfd38b46c5e339e26e
Funny-Buny <incoming@mail.ru>
2021-07-08 13:02:24 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/inheritance/JsonReport.java
+++ src/main/java/inheritance/JsonReport.java
@@ -1,8 +1,9 @@
 package inheritance;
 
 public class JsonReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + name + " : " + name + System.lineSeparator() + body + " : " + body + System.lineSeparator() + "}";
-
     }
 }
