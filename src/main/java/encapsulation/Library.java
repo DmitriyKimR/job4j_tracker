b6a7bafaa6cc9c62dc936ca1c5eaa16621dc6653
Funny-Buny <incoming@mail.ru>
2021-07-12 12:00:20 +0300

    3. Массивы и модели. [#174892 #176099]

--- src/main/java/encapsulation/Library.java
+++ src/main/java/encapsulation/Library.java
@@ -6,10 +6,11 @@ public class Library {
         Book thinking = new Book("Java thinking.", 560);
         Book philosophy = new Book("Java philosophy.", 650);
         Book code = new Book("Java code.",830);
+        Book cleanCode = new Book("Clean code", 1200);
         Book[] cleaCode = new Book[4];
         cleaCode[0] = entry;
         cleaCode[1] = thinking;
-        cleaCode[2] = philosophy;
+        cleaCode[2] = cleanCode;
         cleaCode[3] = code;
         for (Book index : cleaCode) {
             System.out.println(index.getName() + " with number of pages " + index.getNumber());
@@ -18,7 +19,9 @@ public class Library {
         cleaCode[0] = cleaCode[3];
         cleaCode[3] = temp;
         for (Book index : cleaCode) {
-            System.out.println(index.getName() + " with number of pages " + index.getNumber() + " Clean code");
+            if (index == cleanCode) {
+                System.out.println(index.getName() + " with number of pages " + index.getNumber());
+            }
         }
     }
 }
