34c101361957687c0d86b8b4e56b82a8644e503b
Funny-Buny <incoming@mail.ru>
2021-07-12 12:18:55 +0300

    3.2. Удаление моделей из массива. [#310016 #176708]

--- src/main/java/encapsulation/ShopDrop.java
+++ src/main/java/encapsulation/ShopDrop.java
@@ -2,14 +2,10 @@ package encapsulation;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        products[index] = null;
-        for (int i = 0; i < products.length; i++) {
-            if (products[i] == null && i + 1 < products.length && products[i + 1] != null) {
-                Product temp = products[i];
-                products[i] = products[i + 1];
-                products[i + 1] = temp;
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
             }
-        }
+        products[products.length - 1 ] = null;
         return products;
     }
 }
