2c9c326ab6d374017ffa2354224d8ebf82100ba6
Funny-Buny <incoming@mail.ru>
2021-07-12 09:29:29 +0300

    3.1. Массив с пустыми ячейками [#176470 #176118]

--- src/main/java/encapsulation/Shop.java
+++ src/main/java/encapsulation/Shop.java
@@ -2,13 +2,10 @@ package encapsulation;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int rsl = 0;
+        int rsl = -1;
         for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
                 rsl = i;
-                break;
-            } else if (products.length <= 1) {
-                rsl = -1;
                 break;
             }
         }
