c4276be83e3039853caa2404eb51ea8604602a7b
Funny-Buny <incoming@mail.ru>
2021-08-16 14:18:32 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,15 +29,29 @@ public class Tracker {
         return Arrays.copyOf(findByName, rsl);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+    private int indexOf(int id) {
+        int rsl = -1;
+        for(int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int i = indexOf(id);
+        if (i != -1) {
+            item.setId(id);
+            items[i] = item;
+            return true;
+        }
+        return false;
     }
 }
