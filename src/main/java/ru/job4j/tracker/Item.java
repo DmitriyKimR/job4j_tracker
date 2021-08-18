726930f020fbcedfae91bee05c4e67cc90a1c300
Funny-Buny <incoming@mail.ru>
2021-08-18 10:03:26 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 import static java.time.LocalDateTime.now;
@@ -9,6 +10,7 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
 
--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -7,9 +7,6 @@ public class StartUi {
 
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime date = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String endFormat = date.format(formatter);
-        System.out.println(endFormat);
+        System.out.println(item);
     }
 }
