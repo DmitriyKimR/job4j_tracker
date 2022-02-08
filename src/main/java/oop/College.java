79a9bb9dab054a947f69a1b3e0c339b47cc6fab6
Funny-Buny <incoming@mail.ru>
2022-02-08 08:49:15 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#080220220849]

---
+++ src/main/java/oop/College.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman ivan = new Freshman();
+        Student st = ivan;
+        Object obj = ivan;
+    }
+}
