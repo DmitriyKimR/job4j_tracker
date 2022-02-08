178561bc10cec699b94098566e21fa6ebc1604c0
Funny-Buny <incoming@mail.ru>
2022-02-08 13:47:48 +0300

    3. Интерфейс Input [#080220221347]

--- src/main/java/polymorphism/Bus.java
+++ src/main/java/polymorphism/Bus.java
@@ -4,11 +4,13 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
+        System.out.println("Передвигается по дроге.");
 
     }
 
     @Override
     public void passenger(int number) {
+        System.out.println("Перевозит" + number + " пассажиров");
 
     }
 
