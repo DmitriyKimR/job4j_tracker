2482f1f213b0a3a360cab5a224d6599ad0fc670f
Funny-Buny <incoming@mail.ru>
2022-02-15 15:06:48 +0300

    5.1. Тестирование. Подготовка данных [#150220221502]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -24,10 +24,26 @@ public class StartUITest {
 
     @Test
     public void replaceItemById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), "edited item" };
+        StartUI.replaceItemById(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
     }
 
     @Test
     public void deleteItemById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("item for delete");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), null};
+        StartUI.deleteItemById(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
     }
 
     @Test
