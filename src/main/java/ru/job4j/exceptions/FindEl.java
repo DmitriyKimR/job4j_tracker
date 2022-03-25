ddaa080495c3b7053d9ff0b9cf15d12190feb765
Funny-Buny <incoming@mail.ru>
2022-03-25 10:44:34 +0300

    0.4. Пользовательские исключения. [#250320221044]

---
+++ src/main/java/ru/job4j/exceptions/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.exceptions;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+            }
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Key is not found in array");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"on", "off", "switch"}, "temp");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
