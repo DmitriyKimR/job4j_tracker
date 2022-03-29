029be64e5274970bd280bbd27787d6210bf8d224
Funny-Buny <incoming@mail.ru>
2022-03-29 10:02:21 +0300

    1.1. Тесты на StartUI. [#290320221002]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -167,12 +167,29 @@ public class StartUITest {
     public void whenExitProgramOutputSuccessful() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        Input in = new StubInput(new String[] {"0"});
-        UserAction[] actions = new UserAction[] {new ExitAction(out)};
+        Input in = new StubInput(new String[]{"0"});
+        UserAction[] actions = new UserAction[]{new ExitAction(out)};
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString(), is(
                 "Menu:" + ln
+                        + "0. Exit Program" + ln
+        ));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input in = new StubInput(new String[]{"5", "0"});
+        UserAction[] actions = new UserAction[]{new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
                         + "0. Exit Program" + ln
         ));
     }
