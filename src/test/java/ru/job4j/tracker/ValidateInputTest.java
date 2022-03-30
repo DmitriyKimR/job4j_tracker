9515bfeb754d8f59e0aa5171dd28fa9412f33212
Funny-Buny <incoming@mail.ru>
2022-03-30 11:22:02 +0300

    2.1. Тест на ValidateInput  [#300320221121]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int select = input.askInt("Enter menu:");
+        assertThat(select, is(1));
+    }
+
+    @Test
+    public void whenFewInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"2", "1", "4", "-2"});
+        ValidateInput input = new ValidateInput(out, in);
+        int select = input.askInt("Enter menu:");
+        assertThat(select, is(2));
+        select = input.askInt("Enter menu:");
+        assertThat(select, is(1));
+        select = input.askInt("Enter menu:");
+        assertThat(select, is(4));
+        select = input.askInt("Enter menu:");
+        assertThat(select, is(-2));
+    }
+
+}
