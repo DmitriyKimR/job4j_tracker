e108a6b6980a4835ef2c0446aeca0e3588ec8979
DmitriyKimR <incoming@mail.ru>
2023-07-20 14:47:47 +0500

    2. Идентичные тексты  [200720231447]

---
+++ src/main/java/ru/job4j/collection/UniqueTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueTest {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String originString : origin) {
+            check.add(originString);
+            for (String textString : text) {
+                if (check.contains(textString)) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/UniqueTestTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueTestTest {
+
+    @Test
+    public void isEquals() {
+        UniqueTest uniqueTest = new UniqueTest();
+        String origin = "My cat eats a mouse";
+        String text = "My cat eats a mouse";
+        assertTrue(uniqueTest.isEquals(origin, text));
+    }
+
+    @Test
+    public void isNotEqual() {
+        UniqueTest uniqueTest = new UniqueTest();
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by a cat";
+        assertTrue(uniqueTest.isEquals(origin, text));
+    }
+}
