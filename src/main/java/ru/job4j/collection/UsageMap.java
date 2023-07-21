e50279ade5ce8b7b91db7b8b820cb8c02976e10a
DmitriyKimR <incoming@mail.ru>
2023-07-21 14:34:39 +0500

    0. Map, HashMap [210720231434]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Oleg Arsentev");
+        map.put("lavrov@yandex.ru", "Sergey Lavrov");
+        map.put("ivanov@yandex.ru", "Petr Ivanov");
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         for (String key : map.keySet()) {
             String value = map.get(key);
