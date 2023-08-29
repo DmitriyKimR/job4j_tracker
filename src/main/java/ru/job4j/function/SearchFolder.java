2a9697c6068ebf7b1d7e13f3f9798fc3064f00f4
DmitriyKimR <incoming@mail.ru>
2023-08-29 14:58:49 +0500

    2.3. Встроенные функциональные интерфейсы. [290820231458]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -8,7 +8,6 @@ public class SearchFolder {
 
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
         List<Folder> result = new ArrayList<>();
-        predicate = str -> str.getSize() > 100 || str.getName().contains("bug");
         for (Folder folder : list) {
             if (predicate.test(folder)) {
                 result.add(folder);
